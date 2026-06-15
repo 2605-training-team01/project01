89b78e74419b527bbe5a817052f8c9403f309c7c
khs4git3-test <emmakangson@gmail.com>
2026-06-15 10:14:43 +0900

    main.py수정

--- python/main.py
+++ python/main.py
@@ -1,14 +1,11 @@
+# app.py
 import streamlit as st
-import pandas as pd
+import time
 import pymysql
 from dotenv import load_dotenv
 import os
 
-# ------------------
-# DB 연결
-# ------------------
-
-env_path='./.env'
+env_path='./test.env'
 load_dotenv(dotenv_path=env_path)
 
 HOST=os.getenv('DB_HOST')
@@ -25,440 +22,382 @@ conn = pymysql.connect(
     charset="utf8"
 )
 
-cursor = conn.cursor()
+cursor = conn.cursor(pymysql.cursors.DictCursor)
 
-
-# ------------------
-# 1단계 : 카테고리 조회
-# ------------------
-st.title("☕ 카페 키오스크")
-
-sql = """
-SELECT category_code,
-       category_name
-FROM CATEGORY
-ORDER BY category_code
-"""
-
-category_df = pd.read_sql(sql, conn)
-
-st.subheader("메뉴선택")
-
-selected_category = st.selectbox(
-    "카테고리 선택",
-    category_df["category_name"]
+st.set_page_config(
+    page_title="키오스크",
+    layout="wide"
 )
 
+# -------------------------
+# 초기 상태
+# -------------------------
+if "page" not in st.session_state:
+    st.session_state.page = "waiting"
 
-# ------------------
-# 2단계 : 선택한 카테고리 메뉴 조회
-# ------------------
-menu_sql = f"""
-SELECT
-    menu_id,
-    menu_name,
-    menu_price
-FROM MENU
-WHERE category_code =
-(
-    SELECT category_code
-    FROM CATEGORY
-    WHERE category_name = '{selected_category}'
-)
-"""
-
-menu_df = pd.read_sql(menu_sql, conn)
-
-# # ------------------
-# # 3단계 : 메뉴 출력
-# # ------------------
-
-
-# st.subheader("메뉴")
-
-# for _, row in menu_df.iterrows():
-
-#     col1, col2 = st.columns([3,1])
-
-#     with col1:
-#         st.write(row["menu_name"])
-
-#     with col2:
-#         st.write(f"{row['menu_price']}원")
-
-
-# ------------------
-# 4단계 : 메뉴 선택
-# ------------------
-if "selected_menu" not in st.session_state:
-    st.session_state.selected_menu = None
-
-for _, row in menu_df.iterrows():
-
-    col1,col2,col3 = st.columns([3,2,1])
-
-    with col1:
-        st.write(row["menu_name"])
-
-    with col2:
-        st.write(f"{row['menu_price']}원")
-
-    with col3:
-        if st.button(
-            "선택",
-            key=row["menu_id"]
-        ):
-            st.session_state.selected_menu = row["menu_id"]
-# 여기수정
-if st.session_state.selected_menu:
-
-    st.success(
-        f"선택된 메뉴 번호 : "
-        f"{st.session_state.selected_menu}"
-    )
-
-quantity = st.number_input(
-    "수량",
-    min_value=1,
-    value=1
-)
-# ------------------
-# 1. 선택된 메뉴의 이름 표시
-# ------------------
-
-if st.session_state.selected_menu:
-
-    sql = """
-    SELECT menu_name,
-           menu_price
-    FROM MENU
-    WHERE menu_id = %s
-    """
-
-    cursor.execute(
-        sql,
-        (st.session_state.selected_menu,)
-    )
-
-    menu_info = cursor.fetchone()
-
-    st.subheader("선택 메뉴")
-
-    st.write(
-        f"{menu_info[0]} "
-        f"({menu_info[1]}원)"
-    )
-
-# ------------------
-# 2. 해당 메뉴의 옵션 조회
-# ------------------
-if st.session_state.selected_menu:
-
-    option_sql = """
-    SELECT
-        og.group_name,
-        o.option_id,
-        o.option_name,
-        o.extra_price
-
-    FROM MENU_OPTION_GROUP mog
-
-    JOIN OPTION_GROUP og
-    ON mog.group_id = og.group_id
-
-    JOIN OPTION o
-    ON og.group_id = o.group_id
-
-    WHERE mog.menu_id = %s
-
-    ORDER BY og.group_id
-    """
-
-    option_df = pd.read_sql(
-        option_sql,
-        conn,
-        params=(st.session_state.selected_menu,)
-    )
-
-# ------------------
-# 3. 옵션 화면 생성
-# ------------------
-if st.session_state.selected_menu:
-    selected_options = []
-    option_total = 0
-
-    for group_name in option_df["group_name"].unique():
-
-        st.write(f"### {group_name}")
-
-    group_df = option_df[
-        option_df["group_name"] == group_name
-    ]
-
-    selected = st.radio(
-        label="",
-        options=group_df["option_name"],
-        key=group_name
-    )
-
-    option_row = group_df[
-        group_df["option_name"] == selected
-    ].iloc[0]
-
-    selected_options.append(
-        {
-            "option_id": option_row["option_id"],
-            "option_name": option_row["option_name"],
-            "extra_price": option_row["extra_price"]
-        }
-    )
-
-    option_total += option_row["extra_price"]
-
-# ------------------
-# 4. 총금액 계산
-# ------------------
-if st.session_state.selected_menu:
-    menu_price = menu_info[1]
-
-    # total_price = menu_price + option_total
-    total_price = (menu_price + option_total) * quantity
-
-    st.subheader("금액")
-
-    st.write(f"메뉴금액 : {menu_price}원")
-    st.write(f"옵션금액 : {option_total}원")
-
-    st.success(
-        f"총 금액 : {total_price}원"
-    )
-
-# ------------------
-# 5. 장바구니 생성
-# ------------------
 if "cart" not in st.session_state:
     st.session_state.cart = []
 
-# ------------------
-# 6. 장바구니 담기 버튼
-# ------------------
+if "order_type" not in st.session_state:
+    st.session_state.order_type = ""
 
-if st.button("장바구니 담기"):
-        
-    st.session_state.cart.append(
-        {
-            "menu_id":
-            st.session_state.selected_menu,
+if "membership" not in st.session_state:
+    st.session_state.membership = False
 
-            "menu_name":
-            menu_info[0],
+# -------------------------
+# 샘플 메뉴
+# -------------------------
+cursor.execute("""
+SELECT
+    c.category_name,
+    m.menu_id,
+    m.menu_name,
+    m.menu_price
+FROM menu m
+JOIN category c
+ON m.category_code = c.category_code
+""")
 
-            "menu_price":
-            menu_price,
+menu_rows = cursor.fetchall()
+# 카테고리 생성
+categories = list(
+    set(row["category_name"] for row in menu_rows)
+)
+# -------------------------
+# 대기 화면
+# -------------------------
+if st.session_state.page == "waiting":
 
-            "quantity": 
-            quantity,
-
-            "options":
-            selected_options,
-
-            "total":
-            total_price
-        }
+    st.markdown(
+        "<h1 style='text-align:center;margin-top:200px;'>☕ KIOSK</h1>",
+        unsafe_allow_html=True
     )
 
-    st.success("장바구니에 추가됨")
+    col1, col2, col3 = st.columns([1,2,1])
 
-# ------------------
-# 7. 장바구니 출력
-# ------------------
+    with col2:
+        if st.button(
+            "화면을 터치하세요",
+            use_container_width=True
+        ):
+            st.session_state.page = "order_type"
+            st.rerun()
 
-st.divider()
+# -------------------------
+# 매장 / 포장
+# -------------------------
+elif st.session_state.page == "order_type":
 
-st.subheader("장바구니")
-cart_total = 0
+    st.title("이용 방법 선택")
 
-for item in st.session_state.cart:
+    c1, c2 = st.columns(2)
 
-    st.write(
-        f"☕ {item['menu_name']} "
-        f"({item['total']}원)"
+    with c1:
+        if st.button("매장", use_container_width=True):
+            st.session_state.order_type = "매장"
+            st.session_state.page = "menu"
+            st.rerun()
+
+    with c2:
+        if st.button("포장", use_container_width=True):
+            st.session_state.order_type = "포장"
+            st.session_state.page = "menu"
+            st.rerun()
+
+# -------------------------
+# 메뉴 선택
+# -------------------------
+elif st.session_state.page == "menu":
+
+    st.title("메뉴 선택")
+
+    st.sidebar.write(
+        f"장바구니 : {len(st.session_state.cart)}개"
     )
 
-    for option in item["options"]:
+    if st.sidebar.button("장바구니 보기"):
+        st.session_state.page = "summary"
+        st.rerun()
 
+    category = st.sidebar.radio(
+    "카테고리",
+    categories
+)
+    
+
+    filtered_menu = [
+    row for row in menu_rows
+    if row["category_name"] == category
+]
+
+    for menu in filtered_menu:
+
+        col1, col2, col3 = st.columns([3,1,1])
+
+        with col1:
+            st.write(menu["menu_name"])
+
+        with col2:
+            st.write(f"{menu['menu_price']:,}원")
+
+        with col3:
+            if st.button(
+                    "추가",
+                key=menu["menu_id"]
+            ):
+                st.session_state.selected_menu = menu
+                st.session_state.page = "option"
+                st.rerun()
+
+# -------------------------
+# 옵션 선택
+# -------------------------
+elif st.session_state.page == "option":
+
+    menu = st.session_state.selected_menu
+
+    st.title("옵션 선택")
+
+    st.subheader(menu["menu_name"])
+
+    cursor.execute("""
+    SELECT
+        o.option_id,
+        o.option_name,
+        o.extra_price
+    FROM option o
+    JOIN menu_option_group mog
+        ON o.group_id = mog.group_id
+    WHERE mog.menu_id=%s
+    """,(menu["menu_id"],))
+
+    options = cursor.fetchall()
+
+    selected_options = []
+
+    total_price = menu["menu_price"]
+
+    for op in options:
+
+        if st.checkbox(
+            f"{op['option_name']} (+{op['extra_price']}원)"
+        ):
+            selected_options.append(op)
+            total_price += op["extra_price"]
+
+    st.metric(
+        "금액",
+        f"{total_price:,}원"
+    )
+# #############################여기부터
+    if st.button("장바구니 담기"):
+
+        st.session_state.cart.append({
+            "menu_id": menu["menu_id"],
+            "menu_name": menu["menu_name"],
+            "price": total_price,
+            "options": selected_options
+        })
+
+        st.session_state.page = "menu"
+        st.rerun()
+# -------------------------
+# 주문 확인
+# -------------------------
+
+elif st.session_state.page == "summary":
+
+    st.title("주문 내역")
+
+    total = 0
+
+    for item in st.session_state.cart:
         st.write(
-            f" └ {option['option_name']}"
+        f"{item['menu_name']} / {item['price']:,}원"
         )
+        total += item["price"]
 
-    cart_total += item["total"]
+    st.divider()
 
-    st.success(
-    f"총 주문금액 : {cart_total}원"
-)
-    
-# ------------------
-# 7. 주문내역 확인 화면
-# ------------------
-st.divider()
-st.subheader("주문내역 확인")
+    st.subheader(f"총 금액 : {total:,}원")
 
-cart_total = 0
+    c1, c2 = st.columns(2)
 
-for item in st.session_state.cart:
+    with c1:
+        if st.button("메뉴 추가"):
+            st.session_state.page = "menu"
+            st.rerun()
 
-    st.write(
-        f"{item['menu_name']} x {item['quantity']}"
-    )
+    with c2:
+        if st.button("결제하기"):
+            st.session_state.page = "payment"
+            st.rerun()
 
-    st.write(
-        f"{item['total']}원"
-    )
-
-    cart_total += item['total']
-
-    st.success(
-    f"결제예정금액 : {cart_total}원"
-)
-    
-# ------------------
-# 7. 주문내역 확인 화면
-# ------------------
-if st.button("결제하기"):
-    st.session_state.page = "payment"
-
-if "page" not in st.session_state:
-    st.session_state.page = "menu"
-
-# ------------------
-# 7. 결제
-# ------------------
-
-if st.session_state.page == "payment":
+# -------------------------
+# 결제
+# -------------------------
+elif st.session_state.page == "payment":
 
     st.title("결제")
-# ------------------
-# 7. 포장
-# ------------------
 
-takeout_type = st.radio(
-    "이용방식",
-    ["포장", "매장"]
-)
-
-
-
-
-# ------------------
-# 7. 포장
-# ------------------
-phone_number = st.text_input(
-    "전화번호"
-)
-
-
-
-
-# 조회
-sql = """
-SELECT member_id,
-       stamp,
-       grade
-FROM MEMBER
-WHERE phone_number=%s
-"""
-
-cursor.execute(
-    sql,
-    (phone_number,)
-)
-
-member = cursor.fetchone()
-
-
-if member:
-    st.success(
-        f"{member[2]} 회원"
+    payment = st.radio(
+        "결제 수단",
+        ["카드", "간편결제", "쿠폰"]
     )
 
-# ------------------
-# 7. 결제수단
-# ------------------
-pay_type = st.radio(
-    "결제수단",
-    ["CARD","CASH"]
-)
+    if st.button("결제 완료"):
 
-if st.button("결제 완료"):
-    order_sql = """
-INSERT INTO ORDERS
-(
- member_id,
- takeout_type,
- order_date,
- total_amount
-)
-VALUES
-(
- %s,
- %s,
- NOW(),
- %s
-)
-"""
+        total_amount = sum(
+            item["price"]
+            for item in st.session_state.cart
+        )
 
-cursor.execute(
-    order_sql,
-    (
-        member[0] if member else None,
-        "Y" if takeout_type=="포장" else "N",
-        cart_total
+        cursor.execute("""
+        INSERT INTO orders(
+            member_id,
+            takeout_type,
+            total_amount
+        )
+        VALUES(%s,%s,%s)
+        """,(
+            None,
+            "Y" if st.session_state.order_type=="포장"
+            else "N",
+            total_amount
+        ))
+
+        order_id = cursor.lastrowid
+
+        for item in st.session_state.cart:
+
+            cursor.execute("""
+            INSERT INTO order_detail(
+                order_id,
+                menu_id,
+                quantity,
+                menu_price,
+                amount
+            )
+            VALUES(%s,%s,%s,%s,%s)
+            """,(
+                order_id,
+                item["menu_id"],
+                1,
+                item["price"],
+                item["price"]
+            ))
+
+            detail_id = cursor.lastrowid
+
+            for op in item["options"]:
+
+                cursor.execute("""
+                INSERT INTO order_option(
+                    detail_id,
+                    option_id,
+                    option_price
+                )
+                VALUES(%s,%s,%s)
+                """,(
+                    detail_id,
+                    op["option_id"],
+                    op["extra_price"]
+                ))
+        cursor.execute("""
+            INSERT INTO payment(
+                member_id,
+                order_id,
+                final_amt,
+                pay_date,
+                pay_type
+            )
+            VALUES(%s,%s,%s,NOW(),%s)
+            """,(
+                None,
+                order_id,
+                total_amount,
+                payment
+            ))
+        conn.commit()
+
+        st.session_state.payment = payment
+        st.session_state.page = "membership"
+        st.rerun()
+    
+# -------------------------
+# 멤버십
+# -------------------------
+elif st.session_state.page == "membership":
+
+    st.title("멤버십 적립")
+
+    c1, c2 = st.columns(2)
+
+    with c1:
+        if st.button("적립 안함"):
+            st.session_state.page = "receipt"
+            st.rerun()
+
+    with c2:
+        if st.button("적립하기"):
+            st.session_state.page = "phone"
+            st.rerun()
+
+# -------------------------
+# 번호 입력
+# -------------------------
+elif st.session_state.page == "phone":
+
+    st.title("휴대폰 번호 입력")
+
+    phone = st.text_input(
+        "휴대폰 번호",
+        placeholder="01012345678"
     )
-)
 
-conn.commit()
+    if st.button("적립"):
+        st.session_state.phone = phone
+        st.session_state.page = "receipt"
+        st.rerun()
 
-order_id = cursor.lastrowid
+# -------------------------
+# 영수증
+# -------------------------
+elif st.session_state.page == "receipt":
 
-# 장바구니 반복
-for item in st.session_state.cart:
-    detail_sql = """
-INSERT INTO ORDER_DETAIL
-(
- order_id,
- menu_id,
- quantity,
- menu_price,
- amount
-)
-VALUES
-(
- %s,
- %s,
- %s,
- %s,
- %s
-)
-"""
+    st.title("영수증 발행")
 
+    c1, c2 = st.columns(2)
 
-payment_sql = """
-INSERT INTO PAYMENT
-(
- member_id,
- order_id,
- final_amt,
- pay_date,
- pay_type
-)
-VALUES
-(
- %s,
- %s,
- %s,
- CURDATE(),
- %s
-)
-"""
+    with c1:
+        if st.button("영수증 발행"):
+            st.session_state.page = "complete"
+            st.rerun()
 
-st.success("주문 완료")
+    with c2:
+        if st.button("발행 안함"):
+            st.session_state.page = "complete"
+            st.rerun()
+
+# -------------------------
+# 완료
+# -------------------------
+elif st.session_state.page == "complete":
+
+    st.success("주문이 완료되었습니다.")
+
+    st.markdown(
+        """
+        ## 주문번호
+        # A-101
+        """
+    )
+
+    st.info("10초 후 초기화")
+
+    time.sleep(10)
+
+    st.session_state.page = "waiting"
+    st.session_state.cart = []
+
+    st.rerun()
