468eebd38beab132e07a8c7fd1a2cfe60d91d9ab
tjwldbs1355-test <tjwldbs1355@gmail.com>
2026-06-18 10:12:59 +0900

    관리자 페이지 로그인 디비 연동

--- kiosk/kiosk_pages/admin_login.py
+++ kiosk/kiosk_pages/admin_login.py
@@ -1,7 +1,14 @@
 import streamlit as st
+from db.database import get_cursor
+
 
 def render():
+
     st.title("관리자 로그인")
+
+    admin_name = st.text_input(
+        "아이디"
+    )
 
     password = st.text_input(
         "비밀번호",
@@ -11,17 +18,40 @@ def render():
     col1, col2 = st.columns(2)
 
     with col1:
+
         if st.button("로그인"):
 
-            if password == "1234":
+            conn, cursor = get_cursor()
+
+            cursor.execute("""
+                SELECT *
+                FROM admin
+                WHERE admin_name = %s
+                AND admin_pw = %s
+            """, (
+                admin_name,
+                password
+            ))
+
+            admin = cursor.fetchone()
+
+            conn.close()
+
+            if admin:
+
                 st.session_state.is_admin = True
                 st.session_state.page = "admin_dashboard"
                 st.rerun()
 
             else:
-                st.error("비밀번호가 틀렸습니다.")
+
+                st.error(
+                    "아이디 또는 비밀번호가 틀렸습니다."
+                )
 
     with col2:
+
         if st.button("취소"):
+
             st.session_state.page = "waiting"
             st.rerun()
