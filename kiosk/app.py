ebb7cb1d5cacbb2d16fcd4baac4bcc742becc9bf
tjwldbs1355-test <tjwldbs1355@gmail.com>
2026-06-18 17:19:09 +0900

    ㅜㅜ

--- kiosk/app.py
+++ kiosk/app.py
@@ -3,9 +3,8 @@ import streamlit as st
 from utils.session import init_session
 from utils.cookies import cookie_manager
 
-
-from in_charge.waiting2 import render as waiting_page
-# from kiosk_pages.waiting import render as waiting_page
+from in_charge.waiting2 import render  as waiting_page
+#from kiosk_pages.waiting import render as waiting_page
 from kiosk_pages.order_type import render as order_type_page
 from kiosk_pages.menu import render as menu_page
 from kiosk_pages.option import render as option_page
